654a855de4bfd3838fa97bb547e260976c19255b
lxj <lixj_zj@163.com>
2019-09-28 23:09:55 +0800

    update mybatis generator config;

---
+++ src/main/resources/mybatis/dao/ImagesMapper.java
@@ -0,0 +1,17 @@
+package mybatis.dao;
+
+import mybatis.beans.Images;
+
+public interface ImagesMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Images record);
+
+    int insertSelective(Images record);
+
+    Images selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Images record);
+
+    int updateByPrimaryKey(Images record);
+}
