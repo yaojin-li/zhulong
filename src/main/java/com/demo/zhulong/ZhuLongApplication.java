d2ad81f9cf64e5b62ad4f3a5c9ad350bd4d726be
lxj <lixj_zj@163.com>
2019-11-05 22:17:13 +0800

    update SpringBootServletInitializer and packaging type;

--- src/main/java/com/demo/zhulong/ZhuLongApplication.java
+++ src/main/java/com/demo/zhulong/ZhuLongApplication.java
@@ -1,5 +1,7 @@
 package com.demo.zhulong;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +12,18 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan(basePackages = {"com.demo.zhulong.base.dao.*"})
 //@EnableScheduling  // 开启定时器
-public class ZhuLongApplication {
+public class ZhuLongApplication extends SpringBootServletInitializer {
+
+    /**
+     * @Description: 注册启动类
+     * @Date:        2019/11/5 22:15
+     * @Params:
+     * @ReturnType:
+     **/
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(ZhuLongApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZhuLongApplication.class, args);
